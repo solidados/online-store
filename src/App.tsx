377a68cb7992d6b7ee82003490d5e55787020aac
solidados <geekbrains.pk@internet.ru>
2024-03-31 13:51:29 +0400

    chore: create Navbar and add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import { CartProvider } from "./context/CartContext.tsx";
+
 function App() {
-  return <div>Shop</div>;
+  return (
+    <CartProvider>
+      <Navbar />
+      <Container className="mb-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </CartProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,56 @@
+import { Button, Container, Nav, Navbar as NavBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { useCart } from "../../context/CartContext.tsx";
+
+const Navbar = () => {
+  const { openCart, cartQuantity } = useCart();
+
+  return (
+    <NavBs className="bg-white shadow-sm mb-3" sticky="top">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart}
+            className="d-flex justify-content-center align-items-center rounded-circle"
+            style={{
+              position: "relative",
+              width: "2.4rem",
+              height: "2.4rem",
+            }}
+            variant="outline-primary"
+          >
+            <FaShoppingCart className="position-absolute fs-4" />
+            <div
+              className="d-flex justify-content-center align-items-center rounded-circle bg-danger text-white"
+              style={{
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                width: "30px",
+                padding: "5px 8px",
+                transform: "translate(75%, 25%)",
+                fontSize: "0.9rem",
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
+      </Container>
+    </NavBs>
+  );
+};
+
+export default Navbar;
